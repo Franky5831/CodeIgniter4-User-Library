a244de4568a93186f3018d8d2f213d9626b0c32c
Francesco <fracianna5@gmail.com>
2025-02-28 12:14:34 +0100

    Feature: allow user to disable routes

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -2,9 +2,18 @@
 
 use CodeIgniter\Router\RouteCollection;
 
+$config = config(\Franky5831\CodeIgniter4UserLibrary\Config\App::class);
+$setUserLibRoutes = $config->setUserLibRoutes;
+
 /**
- * @var RouteCollection $routes
+ * Allows the user to disable the routes
+ * This is useful if the user wants to change the urls, or override the controller used
  */
-$routes->match(['get', 'post'], '/login', '\Franky5831\CodeIgniter4UserLibrary\Controllers\User::login', ["as" => "loginurl"]);
-$routes->match(['get', 'post'], '/logout', '\Franky5831\CodeIgniter4UserLibrary\Controllers\User::logout', ["as" => "logouturl"]);
-$routes->match(['get', 'post'], '/register', '\Franky5831\CodeIgniter4UserLibrary\Controllers\User::register', ["as" => "registerurl"]);
+if ($setUserLibRoutes) {
+	/**
+	 * @var RouteCollection $routes
+	 */
+	$routes->match(['get', 'post'], '/login', '\Franky5831\CodeIgniter4UserLibrary\Controllers\User::login', ["as" => "loginurl"]);
+	$routes->match(['get', 'post'], '/logout', '\Franky5831\CodeIgniter4UserLibrary\Controllers\User::logout', ["as" => "logouturl"]);
+	$routes->match(['get', 'post'], '/register', '\Franky5831\CodeIgniter4UserLibrary\Controllers\User::register', ["as" => "registerurl"]);
+}
